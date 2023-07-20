03d2065105e56a44c573b1ec02a45f6835f4d385
zivkoss <zivcoss@yahoo.com>
2023-07-20 14:16:46 +0200

    line 9 changed in data-s.s.ts

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";    
 
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private RecipeService: RecipeService) {}
+
+    storeRecipes() {
+
+    }
 }
