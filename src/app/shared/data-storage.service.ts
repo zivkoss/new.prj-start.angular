0885ebd4e208041dd6a5a59ad4dca8f226908b21
zivkoss <zivcoss@yahoo.com>
2023-07-20 14:34:02 +0200

    Recipe import removed changed in data-s.s.ts

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";    
 
-import { Recipe } from '../recipes/recipe.model';
+// import { Recipe } from '../recipes/recipe.model'; // removed
 import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient, private RecipeService: RecipeService) {}
+    constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
     storeRecipes() {
-
+      const recipes = this.recipeService.getRecipes();
+      this.http.put('https://ng-course-recipe-book-83125-default-rtdb.firebaseio.com/recipes.json', recipes);
     }
 }
+
+
+/// ******************  
