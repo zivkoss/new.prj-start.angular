0c74d47a17ba2fdb04c57894e4b32e155702a78f
zivkoss <zivcoss@yahoo.com>
2023-08-09 17:26:16 +0200

    onSubmit(form: NgForm) {         console.log(form.value);         form.reset();     } added in auth.c.ts

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,10 +9,11 @@ export class AuthComponent {
     isLoginMode = true;
 
     onSwitchMode() {
-       this.isLoginMode = !this.isLoginMode;
+        this.isLoginMode = !this.isLoginMode;
     }
 
     onSubmit(form: NgForm) {
-
+        console.log(form.value);
+        form.reset();
     }
 }
