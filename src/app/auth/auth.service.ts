6f51a9655b754ea23a784d48377abe6160ae68b9
zivkoss <zivcoss@yahoo.com>
2023-08-09 18:14:42 +0200

    auth.service.ts interface AuthResponseData {     kind: string;     idToken: string;     email: string;     refreshToken: string;     expiresIn: string;     localId: string; }has been added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 
+interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+}
+
 @Injectable({providedIn: 'root'})
 export class AuthService { 
     constructor(private http: HttpClient) {}
