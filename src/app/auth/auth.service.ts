a90e534f56aa9a2782fff53fb2e2ebb2efdfdaa2
zivkoss <zivcoss@yahoo.com>
2023-08-09 18:11:56 +0200

    auth.service.ts line 8,9,10,11,12,13,14 has bin changed

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,7 +4,14 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({providedIn: 'root'})
 export class AuthService { 
     constructor(private http: HttpClient) {}
-    signup() {
-
+    signup(email: string, password: string) {
+        return this.http.post(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCd-MHgNg2Zn-CB7HdsRuSbuvCZtZVNNqc'
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        );     
     }
 }
