4a9af682056357559b42afc4bab277a2a530e7a8
zivkoss <zivcoss@yahoo.com>
2023-08-30 13:14:26 +0200

    code  added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -33,7 +33,7 @@ export class AuthService {
         )
         .pipe(catchError(this.handleError), tap(resData => {
             const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-            const user = new User(resData.email, resData.localId, resData.idToken);
+            const user = new User(resData.email, resData.localId, resData.idToken, expirationDate);
         }));
     }
 
