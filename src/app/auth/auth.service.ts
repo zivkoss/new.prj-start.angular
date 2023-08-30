425bb0f95c896602f3b32a3d059ae37195f6a3ce
zivkoss <zivcoss@yahoo.com>
2023-08-30 12:26:48 +0200

    imported Subject in auth.service.ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { throwError, Subject } from "rxjs";
 
 export interface AuthResponseData {
     kind: string;
