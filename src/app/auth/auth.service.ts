0775f9fbb4f387971d894676031840f7a9344f1b
zivkoss <zivcoss@yahoo.com>
2023-09-15 18:32:43 +0200

    code  added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -34,7 +34,12 @@ export class AuthService {
         .pipe(
           catchError(this.handleError),
           tap(resData => {
-                this.handleAuthentication(resData.email, resData.localId, resData.expiresIn);
+                this.handleAuthentication(
+                    resData.email, 
+                    resData.localId,
+                    resData.idToken, 
+                    +resData.expiresIn
+                );
             })
         );
     }
