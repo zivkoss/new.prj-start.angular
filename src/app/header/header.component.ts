b323c18739286e273aba88e7eb6709e5c64e7296
zivkoss <zivcoss@yahoo.com>
2023-09-18 18:14:20 +0200

    code  added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,10 @@ import { DataStorageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService, 
+    private authService: AuthService
+  ) {}
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
