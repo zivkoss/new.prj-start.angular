f9754930ccf1c96690365a2ae82046698a54f725
zivkoss <zivcoss@yahoo.com>
2023-09-20 10:26:36 +0200

    code  added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -39,7 +39,7 @@ export class AuthService {
                     resData.idToken,
                     +resData.expiresIn
                 );
-          })
+            })
         );
     }
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -27,6 +27,7 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
+      this.authService.user.pipe(take).subscribe()
        return this.http
         .get<Recipe[]>(
             'https://ng-course-recipe-book-83125-default-rtdb.firebaseio.com/recipes.json'
