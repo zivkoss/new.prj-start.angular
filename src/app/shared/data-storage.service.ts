2b9ebc52c0e9b7489b8dff52f3c1a3e40e79068c
zivkoss <zivcoss@yahoo.com>
2023-09-20 10:16:21 +0200

    code  added

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,10 +4,15 @@ import { map, tap } from "rxjs/operators";
 // import { Recipe } from '../recipes/recipe.model'; // removed
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+    constructor(
+      private http: HttpClient, 
+      private recipeService: RecipeService,
+      private authService: AuthService
+    ) {}
 
     storeRecipes() {
       const recipes = this.recipeService.getRecipes();
