a85db9a307d07e9f890e1ecc0ddddc145e1ffc9a
zivkoss <zivcoss@yahoo.com>
2023-09-20 10:27:26 +0200

    code  added

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";    
-import { map, tap } from "rxjs/operators";
+import { map, tap, take } from "rxjs/operators";
 // import { Recipe } from '../recipes/recipe.model'; // removed
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
