b13470113ffd92c83c6ab6a23bb70f37597ddf3a
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-04-21 22:20:45 +0200

    add new ad empty array in recipe-l.c.ts

--- course-project-starting-project/src/app/recipes/recipes.component.ts
+++ course-project-starting-project/src/app/recipes/recipes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './recipes.component.css'
 })
 export class RecipesComponent {
+ 
 
 }
