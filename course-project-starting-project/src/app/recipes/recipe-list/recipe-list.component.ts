87d9adf7b5b087989fc64259e54b3c919cadec4f
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-04-21 22:34:55 +0200

    import { Recipe } from '../recipe.model' in recipe-list.c.ts

--- course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [];
 
   constructor() { }
 
