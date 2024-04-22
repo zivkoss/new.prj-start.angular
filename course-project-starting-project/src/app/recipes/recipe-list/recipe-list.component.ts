e507cdea24483312ba919590500cb19430205646
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-04-22 09:28:38 +0200

    add new code

--- course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project-starting-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    // new Recipe('A Test Recipe', 'This is simply a terst', )
+    new Recipe('A Test Recipe', 'This is simply a terst', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
 
   constructor() { }
