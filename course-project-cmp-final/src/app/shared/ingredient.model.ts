3525137db783aeb0c92dd7bdd774fe05517ad995
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-04-23 11:43:22 +0200

    // Typescript schortcut export class Ingredient {   constructor(public name: string, public amount: number) {} }

--- course-project-cmp-final/src/app/shared/ingredient.model.ts
+++ course-project-cmp-final/src/app/shared/ingredient.model.ts
@@ -1,9 +1,22 @@
+// Typescript schortcut 
 export class Ingredient {
-  public name: string;
-  public amount: number;
-
-  constructor(name: string, amount: number) {
-    this.name = name;
-    this.amount = amount;
-  }
+  constructor(public name: string, public amount: number) {}
 }
+
+
+
+
+
+
+
+
+
+// export class Ingredient {
+//   public name: string;
+//   public amount: number;
+
+//   constructor(name: string, amount: number) {
+//     this.name = name;
+//     this.amount = amount;
+//   }
+// }
