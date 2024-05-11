75a1ebf09e81c26f68a5442b9bbc0e9f9857d4c1
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-11 10:47:31 +0200

    export class HeaderComponent {   featureSelected = new EventEmitter in headre.c.ts

--- course-project-cmp-final/src/app/header/header.component.ts
+++ course-project-cmp-final/src/app/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  featureSelected = new EventEmitter
+
   onSelect(feature: string) {
 
   }
