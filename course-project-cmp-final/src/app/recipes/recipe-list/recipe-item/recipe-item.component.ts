da2d37361ce27e97c3d82e65a84167b0d162f9af
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-13 09:39:49 +0200

    add new code

--- course-project-cmp-final/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ course-project-cmp-final/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
 
@@ -9,7 +9,7 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-  recipeSelected = new EventEmitter<void>();
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() { }
 
