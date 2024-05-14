7959fe5fbec164363202c034e7ee125c6799416c
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-14 21:28:16 +0200

    add new code

--- course-project-cmp-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project-cmp-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput')  nameInputRef: ElementRef;
+  @ViewChild('amountInput')  amountInputRef: ElementRef;
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem() {
+
   }
 
 }
