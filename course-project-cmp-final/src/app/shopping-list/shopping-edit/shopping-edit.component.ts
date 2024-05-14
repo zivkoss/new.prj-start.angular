8c45c82d35f07f7d977a51d99d356720e8f6168b
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-14 21:50:24 +0200

    add new code

--- course-project-cmp-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project-cmp-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild, EventEmitter } from '@angular/core';
 
+import { Ingredient } from '../../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
@@ -8,7 +10,7 @@ import { Component, OnInit, ElementRef, ViewChild, EventEmitter } from '@angular
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput')  nameInputRef: ElementRef;
   @ViewChild('amountInput')  amountInputRef: ElementRef;
-  ingredientAdded = new EventEmitter<{name: string, amount: number}>();
+  ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
