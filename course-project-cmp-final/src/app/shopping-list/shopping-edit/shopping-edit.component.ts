4d4dd21d8f97250ecc5ccea1cb649a71b48bfed8
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-14 21:58:04 +0200

    add new code

--- course-project-cmp-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project-cmp-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -21,6 +21,7 @@ export class ShoppingEditComponent implements OnInit {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
