dee2e78a6cd9c08322a1b1872eafe3fe25c11c73
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-15 15:53:29 +0200

    add new code

--- course-project-cmp-final/src/app/shared/dropdown.directive.ts
+++ course-project-cmp-final/src/app/shared/dropdown.directive.ts
@@ -1,9 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'  
 })
 
 export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
 
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 }
