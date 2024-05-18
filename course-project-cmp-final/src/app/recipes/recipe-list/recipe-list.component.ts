16d198853680e19523446b13c411e63723bc6c8a
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-18 19:58:23 +0200

    add line 22 in recipe-list.c.ts -- constructor(private recipeService: RecipeService) {   }

--- course-project-cmp-final/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project-cmp-final/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -18,7 +19,8 @@ export class RecipeListComponent implements OnInit {
 
   ;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { 
+  }
 
   ngOnInit() {
   }
