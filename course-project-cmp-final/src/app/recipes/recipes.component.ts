1e49895d282f14f0c1df91e3f8a870aae9e9988a
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-18 20:22:17 +0200

    add new code

--- course-project-cmp-final/src/app/recipes/recipes.component.ts
+++ course-project-cmp-final/src/app/recipes/recipes.component.ts
@@ -12,7 +12,7 @@ import { RecipeService } from './recipe.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;  
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
