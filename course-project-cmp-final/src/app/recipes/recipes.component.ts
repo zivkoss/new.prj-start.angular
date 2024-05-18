df104e4a85538bc2c270f8b49858d51210b33275
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-18 18:19:16 +0200

    add new code

--- course-project-cmp-final/src/app/recipes/recipes.component.ts
+++ course-project-cmp-final/src/app/recipes/recipes.component.ts
@@ -5,7 +5,8 @@ import { Recipe } from './recipe.model';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
+  providers: []
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;  
