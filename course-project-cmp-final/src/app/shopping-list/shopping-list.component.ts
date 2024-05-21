71f38ee779643387ce052318f19bca6b359df92f
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-21 15:10:10 +0200

    add new code

--- course-project-cmp-final/src/app/shopping-list/shopping-list.component.ts
+++ course-project-cmp-final/src/app/shopping-list/shopping-list.component.ts
@@ -15,5 +15,11 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged
+      .subscribe(
+        (ingredients: Ingredient[]) => {
+          this.ingredients = ingredients;
+        }
+      );
   }
 }
