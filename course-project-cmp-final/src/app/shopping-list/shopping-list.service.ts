84a533744c36d23b1434de4012e2e0ade09063a2
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-21 14:14:23 +0200

    add new code

--- course-project-cmp-final/src/app/shopping-list/shopping-list.service.ts
+++ course-project-cmp-final/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,12 @@
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
-    ingredients: Ingredient[] = [
+    private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
-        new Ingredient('Tomatoes', 10),
-      ];
+        new Ingredient('Tomatos', 10),
+    ];
     
+    getIngredients() {
+        return this.ingredients.slice();
+    }
 } 
