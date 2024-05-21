eebadb59e9fe29e23fbc9004ca2c9a4adc284fe5
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-21 14:28:54 +0200

    add new code

--- course-project-cmp-final/src/app/shopping-list/shopping-list.component.ts
+++ course-project-cmp-final/src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -10,7 +11,7 @@ import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
