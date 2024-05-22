5af9c67cad13b4ce82c299d2f537802323e8e559
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-22 18:26:29 +0200

    add new code

--- course-project-cmp-final/src/app/recipes/recipe.service.ts
+++ course-project-cmp-final/src/app/recipes/recipe.service.ts
@@ -1,13 +1,27 @@
 import { EventEmitter } from "@angular/core";
 
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe(
+          'Tasty Schnitzel', 
+          'This is simply a test', 
+          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+        new Recipe('Big Fat Burger', 
+          'This is simply a test', 
+          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
       ];
     
       getRecipes() {
