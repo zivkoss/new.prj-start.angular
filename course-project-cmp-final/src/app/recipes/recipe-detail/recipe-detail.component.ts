5f71dc9eb69b8b4093fb2efaa038af2266935fe6
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-22 23:29:03 +0200

    add new code

--- course-project-cmp-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ course-project-cmp-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,12 +11,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
   
   onAddToShoppingList() {
-    
+
   }
 }
