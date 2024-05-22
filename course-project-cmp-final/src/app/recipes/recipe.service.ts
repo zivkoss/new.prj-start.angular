d583b406faf02e82a3e9ffbbdadfb635e72dc3e0
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-22 23:20:32 +0200

    add new code

--- course-project-cmp-final/src/app/recipes/recipe.service.ts
+++ course-project-cmp-final/src/app/recipes/recipe.service.ts
@@ -10,14 +10,14 @@ export class RecipeService {
         new Recipe(
           'Tasty Schnitzel', 
           'This is simply a test', 
-          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+          'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
         new Recipe('Big Fat Burger', 
           'This is simply a test', 
-          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+          'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
       [
         new Ingredient('Buns', 2),
         new Ingredient('Meat', 1)
